813d51e0698362d5f20d8e67be4151d9cd4da0ec
giorgideveloper <gorgiladze888@gmail.com>
2022-04-14 17:30:53 +0400

    add material ui

---
+++ src/component/app/App.jsx
@@ -0,0 +1,15 @@
+import BasicAlerts from '../alert/Alert';
+import BasicSpeedDial from '../dial/Dial';
+import BasicExampleDataGrid from '../forms/Forms';
+import './App.css';
+function App() {
+	return (
+		<div className='App'>
+			<BasicAlerts />
+			<BasicExampleDataGrid />
+			<BasicSpeedDial />
+		</div>
+	);
+}
+
+export default App;
